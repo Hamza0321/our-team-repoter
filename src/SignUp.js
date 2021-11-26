8bf5a8b488db5b1dcef0089779a9837d565d2c96
shaheer9294 <shaheer9294@gmail.com>
2021-11-26 22:56:39 +0500

    singup created

---
+++ src/SignUp.js
@@ -0,0 +1,73 @@
+import react from 'react'
+
+const SignUp = () => {
+    return (
+            <div>
+                 <div>
+                     <h2 className = 'form-title'>Sign Up</h2>
+                     <form className ="registration-form" id = "registration-form">
+
+                         <div className="form-group">
+                             <label htmlFor='name'>
+                             <i class="zmdi zmdi-account material-icon-name"></i>
+                             </label>
+                             <input type="text" name="name" id="name" autoCapitalize="off"
+                             placeholder="Your Name"/>
+                         </div>
+                         
+                         <div className="form-group">
+                             <label htmlFor='name'>
+                             <i class="zmdi zmdi-account material-icon-name"></i>
+                             </label>
+                             <input type="text" name="name" id="name" autoCapitalize="off"
+                             placeholder="Your Name"/>
+                         </div>
+
+                         <div className="form-group">
+                             <label htmlFor='name'>
+                             <i class="zmdi zmdi-account material-icon-name"></i>
+                             </label>
+                             <input type="text" name="name" id="name" autoCapitalize="off"
+                             placeholder="Your Name"/>
+                         </div>
+
+                         <div className="form-group">
+                             <label htmlFor='name'>
+                             <i class="zmdi zmdi-account material-icon-name"></i>
+                             </label>
+                             <input type="text" name="name" id="name" autoCapitalize="off"
+                             placeholder="Your Name"/>
+                         </div>
+
+                         <div className="form-group">
+                             <label htmlFor='name'>
+                             <i class="zmdi zmdi-account material-icon-name"></i>
+                             </label>
+                             <input type="text" name="name" id="name" autoCapitalize="off"
+                             placeholder="Your Name"/>
+                         </div>
+
+                         <div className="form-group">
+                             <label htmlFor='name'>
+                             <i class="zmdi zmdi-account material-icon-name"></i>
+                             </label>
+                             <input type="text" name="name" id="name" autoCapitalize="off"
+                             placeholder="Your Name"/>
+                         </div>
+
+                         <div className="form-group">
+                             <label htmlFor='name'>
+                             <i class="zmdi zmdi-account material-icon-name"></i>
+                             </label>
+                             <input type="text" name="name" id="name" autoCapitalize="off"
+                             placeholder="Your Name"/>
+                         </div>
+
+                         
+                     </form>
+                 </div>
+            </div>
+            
+    )
+}
+export default SignUp
